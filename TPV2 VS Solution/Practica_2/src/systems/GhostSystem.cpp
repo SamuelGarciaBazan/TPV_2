cf2aa49a6f76e623454960dfe40efd679528b4db
samuel <samuga05@ucm.es>
2024-04-02 16:11:33 +0200

    generacion aleatoria de fantasmas

--- TPV2 VS Solution/Practica_2/src/systems/GhostSystem.cpp
+++ TPV2 VS Solution/Practica_2/src/systems/GhostSystem.cpp
@@ -51,6 +51,8 @@ void GhostSystem::generateGhosts()
 
 	if (lastSpawn + ghostSpawnRate < sdlutils().virtualTimer().currTime()) {
 
+		lastSpawn = sdlutils().virtualTimer().currTime();
+		
 		//creacion del nuevo fantasma
 		auto newGhost = mngr_->addEntity(ecs::grp::GHOSTS);
 
@@ -60,17 +62,53 @@ void GhostSystem::generateGhosts()
 		int fils = 8;
 		int cols = 8;
 
-
-
 		auto tf = mngr_->addComponent<Transform>(newGhost);
 		auto img = mngr_->addComponent<ImageWithFrames>(newGhost,"spriteSheet",fils,cols);
 
 		tf->setWidth(width);
 		tf->setHeight(height);
 
+		int rand = sdlutils().rand().nextInt(0, 4);
 
-		img->firstIndex = 32;
-		img->lastIndex = 39;
+		//colocar el fantasama en una esquina aleatoria
+		switch (rand)
+		{
+		case 0:
+			img->firstIndex = 32;
+			img->lastIndex = 39;
+			img->currentFrame = 32;
+
+			tf->getPos().set(0, 0);
+
+			break;
+		case 1:
+			img->firstIndex = 40;
+			img->lastIndex = 47;
+			img->currentFrame = 40;
+
+			tf->getPos().set(0, sdlutils().height()-height);
+
+			break;
+		case 2:
+			img->firstIndex = 48;
+			img->lastIndex = 55;
+			img->currentFrame = 48;
+
+			tf->getPos().set(sdlutils().width() -width, 0);
+
+			break;
+		case 3:
+			img->firstIndex = 56;
+			img->lastIndex = 63;
+			img->currentFrame = 56;
+
+			tf->getPos().set(sdlutils().width() - width, sdlutils().height() - height);
+
+			break;
+
+		default:
+			break;
+		}
 
 
 	}
